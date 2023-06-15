0bd099079ce1dc6939a3efc77036c409ec86a67d
Clément Lapasset <clement.lapasset@gmail.com>
2023-06-15 18:48:11 +0200

    Add landscape or portrait option for block images

--- schemas/contentBlock.js
+++ schemas/contentBlock.js
@@ -7,14 +7,29 @@ export default {
       name: 'image',
       title: 'Image',
       type: 'image',
-      description: 'Image associée au bloc de contenu (optionnel)',
+      description: 'Image associée au bloc de contenu',
       options: {hotspot: true},
+    },
+    {
+      name: 'orientation',
+      title: "Orientation de l'image",
+      type: 'string',
+      description:
+        'Une image au format paysage prendra toute la largeur, au format portrait sa largeur sera limitée',
+      options: {
+        list: [
+          {title: 'Paysage', value: 'landscape'},
+          {title: 'Portrait', value: 'portrait'},
+        ],
+        layout: 'radio',
+        defaultValue: 'landscape',
+      },
     },
     {
       name: 'text',
       title: 'Texte',
       type: 'text',
-      description: 'Texte associé au bloc de contenu (optionnel)',
+      description: 'Texte associé au bloc de contenu',
     },
   ],
   preview: {
