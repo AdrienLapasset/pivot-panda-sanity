602023c12a65e14a206aab6086961e17500e4772
AdrienLapasset <aflapasset@gmail.com>
2023-06-16 17:16:34 +0200

    remove blockContent

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,5 @@
-import blockContent from './blockContent'
 import project from './project'
 import contentBlock from './contentBlock'
 import beforeAfterImages from './beforeAfterImages'
 
-export const schemaTypes = [blockContent, project, contentBlock, beforeAfterImages]
+export const schemaTypes = [project, contentBlock, beforeAfterImages]
